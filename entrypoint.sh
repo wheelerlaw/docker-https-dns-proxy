7c9a222e48b1ae6215a1d63172f7f25de97ba89f
Wheeler Law <whelderwheels613@gmail.com>
2018-04-25 01:30:16 -0400

    Added entrypoint scripts

---
+++ entrypoint.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+args=$@
+
+echo "Activating iptables rules..."
+/usr/local/bin/fw.sh start
+
+pid=0
+
+# SIGUSR1 handler
+usr_handler() {
+  echo "usr_handler"
+}
+
+# SIGTERM-handler
+term_handler() {
+    if [ $pid -ne 0 ]; then
+        echo "Term signal catched. Shutdown https_dns_proxy and disable iptables rules..."
+        kill -SIGTERM "$pid"
+        wait "$pid"
+        /usr/local/bin/fw.sh stop
+    fi
+    exit 143; # 128 + 15 -- SIGTERM
+}
+
+# setup handlers
+trap 'kill ${!}; usr_handler' SIGUSR1
+trap 'kill ${!}; term_handler' INT QUIT TERM
+
+echo "Starting redsocks..."
+/usr/local/bin/https_dns_proxy $args &
+pid="$!"
+
+# wait indefinetely
+while true
+do
+    tail -f /dev/null & wait ${!}
+done
---
+++ fw.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+
+##########################
+# Setup the Firewall rules
+##########################
+fw_setup() {
+  # First we added a new chain called 'DOH' to the 'nat' table.
+  iptables -t nat -N DOH
+
+  # We then told iptables to redirect all port 53 connections to the 
+  # DNS-over-HTTPS server listening on 5053
+  iptables -t nat -A DOH -p tcp --dport 53 -j REDIRECT --to-ports 5053
+  iptables -t nat -A DOH -p ucp --dport 53 -j REDIRECT --to-ports 5053
+
+  iptables -t nat -A PREROUTING -i docker0 -p tcp --dport 53 -j DOH
+  iptables -t nat -A PREROUTING -i docker0 -p udp --dport 53 -j DOH
+}
+
+##########################
+# Clear the Firewall rules
+##########################
+fw_clear() {
+  iptables-save | grep -v DOH | iptables-restore
+  #iptables -L -t nat --line-numbers
+  #iptables -t nat -D PREROUTING 2
+}
+
+case "$1" in
+    start)
+        echo -n "Setting DOH firewall rules..."
+        fw_clear
+        fw_setup
+        echo "done."
+        ;;
+    stop)
+        echo -n "Cleaning DOH firewall rules..."
+        fw_clear
+        echo "done."
+        ;;
+    *)
+        echo "Usage: $0 {start|stop}"
+        exit 1
+        ;;
+esac
+exit 0
